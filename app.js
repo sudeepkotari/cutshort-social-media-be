263ec5e9a5e5c127534fb0e77e197683b068b054
sudeep <sudeep@sudeeps-MacBook-Air.local>
2023-04-01 10:57:01 +0530

    Added cors to allow cross domain requests

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const morgan = require("morgan");
 const createError = require("http-errors");
+const cors = require('cors');
 require("dotenv").config();
 require("./helpers/init_mongodb");
 require("./helpers/init_redis");
@@ -13,6 +14,7 @@ const AdminRoute = require("./routes/admin.route");
 const { limiter } = require("./helpers/rate_limit_helper");
 
 const app = express();
+app.use(cors()) // allow frontend domain here
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
